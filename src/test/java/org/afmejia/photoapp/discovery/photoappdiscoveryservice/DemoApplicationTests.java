72dfe26a535f27c97ebbcbd9d0475b05d76bea19
Felipe <andres@rise.agency>
2019-07-02 11:56:28 -0500

    First commit

---
+++ src/test/java/org/afmejia/photoapp/discovery/photoappdiscoveryservice/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.afmejia.photoapp.discovery.photoappdiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
